6ca5ffacbf90501cf2d0820c98d8b08f6ca966f7
Taylor Lee <tay333@gmail.com>
2017-04-17 12:53:33 -0700

    fsm.py

---
+++ fsm.py
@@ -0,0 +1,35 @@
+from pygraphviz import AGraph
+
+class FSM(object):
+    '''Finite State Machine'''
+
+    def __init__(self, finite_states):
+        self.states = finite_states
+        nodes = {
+            state : []
+            for state in finite_states
+        }
+        self.graph = AGraph(nodes, directed=True, strict=False)
+        self.edgesize = len(nodes) / 5.0
+        return
+
+    def validate(self, *args):
+        for arg in args:
+            assert arg in self.states, arg
+        return
+
+    def transition(self, src, dest, label='', **kwargs):
+        self.validate(src, dest)
+        self.graph.add_edge(src, dest, label=label, len=self.edgesize, **kwargs)
+        return
+
+    def success(self, src, dest, label=''):
+        self.transition(src, dest, label, color='green')
+
+    def fail(self, src, dest, label=''):
+        self.transition(src, dest, label, color='red')
+
+    def save(self,name='graph',prog='neato'):
+        self.graph.layout(prog=prog)
+        self.graph.draw('{}.png'.format(name))
+        return
